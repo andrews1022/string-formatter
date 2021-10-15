423e1df7796331e8b5288761de45b080b98bfa76
andrews1022 <47801024+andrews1022@users.noreply.github.com>
2021-10-15 14:53:29 -0700

    setup footer component boilerplate global styles

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface FooterProps {}
+
+const Footer = (props: FooterProps) => {
+	return (
+		<div>
+			<h2>Hello from the Footer component!</h2>
+		</div>
+	);
+};
+
+export default Footer;
